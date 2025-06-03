57a0291cbc52d18297438c3caccc8c3c939cf2ba
Johnny <63070273+TJfiles@users.noreply.github.com>
2025-06-03 13:36:25 -0300

    Add files via upload

---
+++ EX_1_cadastro.py
@@ -0,0 +1,20 @@
+# Escreva um programa que pede ao usuário o nome, idade, e-mail e senha para um cadastro e depois exiba as informações na tela:
+
+# OUTPUT ESPERADO:
+
+# | ------------------------------ |
+# | ---------- CADASTRO ---------- |
+# | ------------------------------ |
+# | Nome: Maria
+# | Idade: 17
+# | Email: maria@email.com
+# | Senha: 123123
+
+# | ------------------------------ |
+# | ----- USUÁRIO CADASTRADO ----- |
+# | Seja bem vindo(a) Maria!
+# | Email: maria@email.com
+# | ------------------------------ |
+
+# ------------------------------------------ ESCREVA SEU CÓDIGO ABAIXO -----------------------------------------------------------
+
---
+++ EX_2_soma.py
@@ -0,0 +1,10 @@
+# Escreva um programa que pede ao usuário dois números e exiba no final a soma deles:
+# OUTPUT ESPERADO:
+
+# Digite um número: 10
+# Digite outro número: 30
+# A soma entre 10 e 30 é: 40
+
+
+# ------------------------------------------ ESCREVA SEU CÓDIGO ABAIXO -----------------------------------------------------------
+
